e40f9b7c0d41bd2e28ebf83a9447ccb7e2ba892f
David Rosillo <drr538@gmail.com>
2016-02-19 23:59:52 -0500

    Add code to login with Twitch (not working for now)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'devise/registrations#new'
+  devise_scope :user do
+
+    get :sign_up, to: 'devise/registrations#new'
+    get :sign_in, to: 'devise/sessions#new'
+    get :sign_out, to: 'devise/sessions#destroy'
+
+    # get :change_password, to: 'registrations#change_password'
+    # put :change_password, to: 'registrations#update_password'
+
+    # root to: 'dashboards#landing'
+    # get :search, to: 'dashboards#search'
+    root to: 'devise/registrations#new'
+
+  end
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
 end
