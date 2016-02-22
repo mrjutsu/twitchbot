be710f733b71ed53d8146e3eed52feee05880587
David Rosillo <drr538@gmail.com>
2016-02-22 09:19:11 -0500

    Add Dashboard model

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
+  resources :dashboards
   devise_scope :user do
 
-    get :sign_up, to: 'devise/registrations#new'
-    get :sign_in, to: 'devise/sessions#new'
-    get :sign_out, to: 'devise/sessions#destroy'
+    # get :sign_up, to: 'devise/registrations#new'
+    get :sign_up, to: 'devise/registrations#new'#, :as => :new_user_session
+    delete :sign_out, to: 'devise/sessions#destroy'#, :as => :destroy_user_session
+
+    get :twitch, to: 'devise/sessions#twitch'
 
     # get :change_password, to: 'registrations#change_password'
     # put :change_password, to: 'registrations#update_password'
 
     # root to: 'dashboards#landing'
     # get :search, to: 'dashboards#search'
-    root to: 'devise/registrations#new'
+    root to: 'dashboards#index'
 
   end
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
+
+  devise_for :users, :controllers => { :sessions => "sessions", :registrations => "registrations"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220043546) do
+ActiveRecord::Schema.define(version: 20160220222749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dashboards", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "dashboards", ["user_id"], name: "index_dashboards_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -36,4 +44,5 @@ ActiveRecord::Schema.define(version: 20160220043546) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "dashboards", "users"
 end
