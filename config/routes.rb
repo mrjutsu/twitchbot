54e16629f227609c0c737323fadd9f4754e7a412
David Rosillo <drr538@gmail.com>
2016-02-23 00:14:07 -0500

    Add routes for twitch auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
   resources :dashboards
+
+  devise_for :users, :controllers => { :sessions => "sessions", :registrations => "registrations"}
+
   devise_scope :user do
 
     # get :sign_up, to: 'devise/registrations#new'
     get :sign_up, to: 'devise/registrations#new'#, :as => :new_user_session
+    get :sign_in, to: 'devise/sessions#new'#, :as => :new_user_session
+
+    get :twitch_auth, to: 'sessions#twitch_auth', as: :twitch_auth
+    get :twitch_sign_in, to: 'sessions#twitch_sign_in', as: :twitch_sign_in
+
     delete :sign_out, to: 'devise/sessions#destroy'#, :as => :destroy_user_session
 
-    get :twitch, to: 'devise/sessions#twitch'
 
     # get :change_password, to: 'registrations#change_password'
     # put :change_password, to: 'registrations#update_password'
@@ -18,5 +25,4 @@ Rails.application.routes.draw do
   end
 
 
-  devise_for :users, :controllers => { :sessions => "sessions", :registrations => "registrations"}
 end
