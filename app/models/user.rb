191a3f55b08437b1398bc1f0dc531f511ea6abb6
David Rosillo <drr538@gmail.com>
2016-02-23 10:12:14 -0500

    Add 60 minutes timeout to user session

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable#, :omniauthable, omniauth_providers: [:twitch]
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
 
   has_one :dashboard, dependent: :destroy
 
